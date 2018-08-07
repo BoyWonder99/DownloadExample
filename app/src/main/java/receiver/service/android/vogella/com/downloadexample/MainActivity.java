25cbf56af51d1be1992b363f2941d4db1f017f4f
Ransi De Silva <ransid@blackmagicdesign.com>
2018-08-08 09:29:38 +1000

    Initial commit

---
+++ app/src/main/java/receiver/service/android/vogella/com/downloadexample/MainActivity.java
@@ -0,0 +1,89 @@
+package receiver.service.android.vogella.com.downloadexample;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+// code taken from http://www.vogella.com/tutorials/AndroidServices/article.html
+
+public class MainActivity extends Activity {
+
+    private TextView textView;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                String string = bundle.getString(DownloadService.FILEPATH);
+                int resultCode = bundle.getInt(DownloadService.RESULT);
+                if (resultCode == RESULT_OK) {
+                    Toast.makeText(MainActivity.this,
+                            "Download complete. Download URI: " + string,
+                            Toast.LENGTH_LONG).show();
+                    textView.setText("Download done");
+                } else {
+                    Toast.makeText(MainActivity.this, "Download failed",
+                            Toast.LENGTH_LONG).show();
+                    textView.setText("Download failed");
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.status);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(
+                DownloadService.NOTIFICATION));
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    public void onClick(View view) {
+
+        requestPermission();
+        Intent intent = new Intent(this, DownloadService.class);
+        // add infos for the service which file to download and where to store
+        intent.putExtra(DownloadService.FILENAME, "index.html");
+        intent.putExtra(DownloadService.URL,
+                "http://www.vogella.com/index.html");
+        startService(intent);
+        textView.setText("Service started");
+    }
+
+// code taken from
+// https://stackoverflow.com/questions/47349953/java-io-filenotfoundexception-storage-emulated-0-notes-fact50result-txt-open
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat
+                    .requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_ASK_PERMISSIONS);
+        }
+    }
+}
